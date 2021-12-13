28dcf22176951f88794d96df4e3ea008d27482e2
HearseDev <hearsd@gmail.com>
2021-12-13 04:03:06 -0800

    automation/sileo

--- update.sh
+++ update.sh
@@ -2,27 +2,28 @@
 script_full_path=$(dirname "$0")
 cd $script_full_path || exit 1
 
-rm Packages Packages.bz2 Packages.xz Packages.zst Release Release.gpg
+rm -f Packages Packages.bz2 Packages.xz Packages.zst Release Release.gpg
 
 echo "[Repository] Generating Packages..."
-apt-ftparchive packages ./pool > Packages
+
+echo "[Repository] Creating Docker Container..."
+docker run --name repo -dit -v ${PWD}:/data ubuntu
+
+echo "[Repository] Updating Docker Container and Installing Utils..."
+docker exec -it repo bash -c "(apt-get update; apt-get install apt-utils)"
+
+docker exec -it repo bash -c "(cd data; apt-ftparchive packages ./pool > Packages)"
 zstd -q -c19 Packages > Packages.zst
 xz -c9 Packages > Packages.xz
 bzip2 -c9 Packages > Packages.bz2
 
 echo "[Repository] Generating Release..."
-apt-ftparchive \
-        -o APT::FTPArchive::Release::Origin="Hearse's Repo" \
-        -o APT::FTPArchive::Release::Label="Hearse's Repo" \
-        -o APT::FTPArchive::Release::Suite="stable" \
-        -o APT::FTPArchive::Release::Version="1.0" \
-        -o APT::FTPArchive::Release::Codename="ios" \
-        -o APT::FTPArchive::Release::Architectures="iphoneos-arm" \
-        -o APT::FTPArchive::Release::Components="main" \
-        -o APT::FTPArchive::Release::Description="Hearse's Dump of Tweaks" \
-        release . > Release
+docker exec -it repo bash -c "(cd data; apt-ftparchive -o APT::FTPArchive::Release::Origin=\"Hearse's Repo\" -o APT::FTPArchive::Release::Label=\"Hearse's Repo\" -o APT::FTPArchive::Release::Suite=\"stable\" -o APT::FTPArchive::Release::Version=\"1.0\" -o APT::FTPArchive::Release::Codename=\"ios\" -o APT::FTPArchive::Release::Architectures=\"iphoneos-arm\" -o APT::FTPArchive::Release::Components=\"main\" -o APT::FTPArchive::Release::Description=\"Hearse's Dump of Tweaks\" release . > Release)"
+
+echo "[Repository] Deleting Docker Container"
+docker rm -f repo
+
 
 echo "[Repository] Signing Release using Hearse's GPG Key..."
 gpg -abs -u CA1E55A06D1AB4CB77DE813873A412BA64BC84B9 -o Release.gpg Release
-
 echo "[Repository] Finished"
