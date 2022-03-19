4eea0251445dad0bf625a4e40a6de1d119301912
HearseDev <hearsd@gmail.com>
2022-03-18 17:52:51 -0700

    update

--- update.sh
+++ update.sh
@@ -26,7 +26,7 @@ docker rm -f repo
 
 echo "[Repository] Signing Release using Hearse's GPG Key..."
 # gpg --passphrase-fd 0 -abs -u CA1E55A06D1AB4CB77DE813873A412BA64BC84B9 -o Release.gpg Release < passphrase.txt
-gpg --batch --passphrase-file passphrase.txt --pinentry-mode loopback -abs -u CA1E55A06D1AB4CB77DE813873A412BA64BC84B9 -o Release.gpg Release
+gpg --passphrase-file passphrase.txt --pinentry-mode loopback -abs -u CA1E55A06D1AB4CB77DE813873A412BA64BC84B9 -o Release.gpg Release
 echo "[Repository] Finished"
 
 git add .
