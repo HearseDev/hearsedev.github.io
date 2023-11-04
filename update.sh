2c987c9dceac1496c79e47d9b8fcc8ec55925883
Hearse <28695977+HearseDev@users.noreply.github.com>
2023-11-04 15:33:50 -0700

    update

--- update.sh
+++ update.sh
@@ -14,9 +14,9 @@ echo "[Repository] Updating Docker Container and Installing Utils..."
 docker exec -it repo bash -c "(apt-get update -y; apt-get install apt-utils -y)"
 
 docker exec -it repo bash -c "(cd data; apt-ftparchive packages ./pool > Packages)"
-zstd -q -c19 Packages > Packages.zst
-xz -c9 Packages > Packages.xz
-bzip2 -c9 Packages > Packages.bz2
+zstd -q -c19 Packages >Packages.zst
+xz -c9 Packages >Packages.xz
+bzip2 -c9 Packages >Packages.bz2
 
 echo "[Repository] Generating Release..."
 docker exec -it repo bash -c "(cd data; apt-ftparchive -o APT::FTPArchive::Release::Origin=\"Hearse's Repo\" -o APT::FTPArchive::Release::Label=\"Hearse's Repo\" -o APT::FTPArchive::Release::Suite=\"stable\" -o APT::FTPArchive::Release::Version=\"2.0\" -o APT::FTPArchive::Release::Codename=\"ios\" -o APT::FTPArchive::Release::Architectures=\"iphoneos-arm iphoneos-arm64\" -o APT::FTPArchive::Release::Components=\"main\" -o APT::FTPArchive::Release::Description=\"Hearse's Dump of Tweaks\" release . > Release)"
@@ -24,10 +24,9 @@ docker exec -it repo bash -c "(cd data; apt-ftparchive -o APT::FTPArchive::Relea
 echo "[Repository] Deleting Docker Container"
 docker rm -f repo
 
-
 echo "[Repository] Signing Release using Hearse's GPG Key..."
 # gpg --passphrase-fd 0 -abs -u CA1E55A06D1AB4CB77DE813873A412BA64BC84B9 -o Release.gpg Release < passphrase.txt
-gpg --passphrase-file passphrase.txt --pinentry-mode loopback -abs -u 7AF1C15738ACCB8A49297D0C277952C29EDC5100 -o Release.gpg Release
+gpg --passphrase-file passphrase.txt --pinentry-mode loopback -abs -u DA7A1D2011A2FB3AAB5C3E7E7E0A34850ADE0665 -o Release.gpg Release
 echo "[Repository] Finished"
 
 git add .
